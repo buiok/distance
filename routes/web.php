a6d3f9e4e8a152d74c351bdd1fb1b2c9d21eb6db
buiok <106178856+buiok@users.noreply.github.com>
2022-10-14 14:02:24 +0500

    edit distance.blade.php and routes web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CalculateDistance;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('distance');
+})->name('main');
+
+Route::post('/', [CalculateDistance::class, 'calculate'])->name('calc');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
