e22ad456ac0182712eb727737cd83d8c435aa8ed
buiok <106178856+buiok@users.noreply.github.com>
2022-10-14 14:27:22 +0500

    update vite settings

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,7 +3,9 @@ $(document).ready(function () {
         token: "fbc34e0e4203136a4c33c16edd0a69f2433e01a9",
         type: "ADDRESS",
         onSelect: function(suggestion) {
-            console.log(suggestion);
+            //console.log(suggestion);
+            $('#geo_lat').val(suggestion.data.geo_lat);
+            $('#geo_lon').val(suggestion.data.geo_lon);
         }
     });
 })
