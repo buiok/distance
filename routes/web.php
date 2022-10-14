64963e9d54a53ecb3308f355fb91d11d722a8e04
buiok <106178856+buiok@users.noreply.github.com>
2022-10-14 21:48:34 +0500

    edit routes web.php

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,8 @@
 use App\Http\Controllers\CalculateDistance;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('distance');
+    return view('dist');
 })->name('main');
 
-Route::post('/', [CalculateDistance::class, 'calculate'])->name('calc');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/', [CalculateDistance::class, 'Calculate'])->name('calc');
