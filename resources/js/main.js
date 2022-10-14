a02c62afddd0a2c551da6ca5823925e797580a74
buiok <106178856+buiok@users.noreply.github.com>
2022-10-14 21:47:53 +0500

    edit resources

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
-    $("#address").suggestions({
+    $("#address1").suggestions({
         token: "fbc34e0e4203136a4c33c16edd0a69f2433e01a9",
         type: "ADDRESS",
         onSelect: function(suggestion) {
             //console.log(suggestion);
-            $('#geo_lat').val(suggestion.data.geo_lat);
-            $('#geo_lon').val(suggestion.data.geo_lon);
+            $('#geo_lat1').val(suggestion.data.geo_lat);
+            $('#geo_lon1').val(suggestion.data.geo_lon);
+        }
+    });
+    $("#address2").suggestions({
+        token: "fbc34e0e4203136a4c33c16edd0a69f2433e01a9",
+        type: "ADDRESS",
+        onSelect: function(suggestion) {
+            //console.log(suggestion);
+            $('#geo_lat2').val(suggestion.data.geo_lat);
+            $('#geo_lon2').val(suggestion.data.geo_lon);
         }
     });
 })
